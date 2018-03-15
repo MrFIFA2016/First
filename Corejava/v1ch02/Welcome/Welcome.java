60ff298efba5a7f5607d76ed2c0d30c75665fe35
MrFIFA2016 <1358329789@qq.com>
2018-03-15 14:15:38 +0800

    javaProj
    提交

---
+++ Corejava/v1ch02/Welcome/Welcome.java
@@ -0,0 +1,43 @@
+import static java.lang.Math.*;
+import java.lang.System;
+
+import java.util.*;
+
+/**
+ * 
+ * @author 13583
+ * @see
+ *
+ */
+public class Welcome {
+    public static void main(String[] args) {
+	/*
+	 * String[] greeting = new String[3]; greeting[0] =
+	 * "Welcome to Core Java"; greeting[1] = "by Cay Horstmann"; greeting[2]
+	 * = "and Gary Cornell";
+	 * 
+	 * for (String g : greeting) System.out.println(g); int i = 0; i =
+	 * 100_00_00; char c = '\u005B'; System.out.println(1 == 12 && (1 / 0 ==
+	 * 3)); System.out.println(sqrt(12)); String string = "aa";
+	 * StringBuilder builder = new StringBuilder(); builder.append("aa");
+	 * Scanner scanner = new Scanner(System.in); int i2 = scanner.nextInt();
+	 * scanner.close();
+	 */
+	// Welcome welcome=new Welcome();
+	NameCls cls = new NameCls();
+	cls.Do();
+    }
+}
+
+class NameCls {
+    // public NameCls() {
+    // char c = 'a';
+    // }
+    {
+	System.out.println("³õÊ¼»¯");
+    }
+
+    public void Do() {
+	System.out.println("Êä³ö");
+    }
+}
