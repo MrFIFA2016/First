c01d7b80097bb1508d0ea7e09a9890d5b98c5732
liuB <1358329789@qq.com>
2018-03-15 21:22:00 +0800

    Update Welcome.java
    5678

--- Corejava/v1ch02/Welcome/Welcome.java
+++ Corejava/v1ch02/Welcome/Welcome.java
@@ -36,7 +36,7 @@ class NameCls {
 	System.out.println("然后我又在本地加了一句话！");
 	System.out.println("用vs加了一句话");
 	System.out.println("用乌龟加了一句话");
-	System.out.println("删除后01234");
+	System.out.println("删除后012345678");
     }
 
     public void Do() {
